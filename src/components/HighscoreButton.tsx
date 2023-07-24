6044a7b8565ceee9281535e55cc9bcd277307ad2
ayushmanbt <ayushmanbilasthakur@gmail.com>
2023-07-24 05:19:41 -0700

    Build Success

--- src/components/HighscoreButton.tsx
+++ src/components/HighscoreButton.tsx
@@ -1,33 +1,36 @@
 import { mdiReload } from '@mdi/js'
 import Icon from '@mdi/react'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { setHighScore } from '../store/scoreSlice';
 
 function HighscoreButton() {
 
+  const [displayDialog, setDisplayDialog] = useState(false);
+ 
   const dialogRef = useRef<HTMLDialogElement>(null);
   const dispatch = useDispatch();
   const resetHighscore = () => {
     
     dispatch(setHighScore(0));
-    dialogRef.current?.close();
+    // dialogRef.current?.close();
+    setDisplayDialog(false);
   }
 
 
   return (
     <>
 
-        <dialog ref={dialogRef}>
+       {displayDialog && <dialog open>
             Are you sure you want to reset highscore?
             <div className="flex">
                 <button className='btn btn-green' onClick={resetHighscore}>Yes</button>
-                <button className='btn btn-red' onClick={() => dialogRef.current?.close()}>No</button>
+                <button className='btn btn-red' onClick={() => setDisplayDialog(false)}>No</button>
             </div>
         </dialog> 
-        
+      }
 
-        <button className="btn btn-red" onClick={() => dialogRef.current?.showModal()}>
+        <button className="btn btn-red" onClick={() => setDisplayDialog(true)}>
             <Icon 
                 path = {mdiReload}
                 size = {0.7}
